c701f1c2ef64e72a300101123e0652d307f9da69
ChenYue Zhang <CZhang13@slb.com>
2016-10-13 00:08:30 +0800

    [CY] add page footer

--- app/src/components/PageFooter.js
+++ app/src/components/PageFooter.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import "../assets/styles/PageFooter.css"
+import codeImg from "../assets/images/code.png"
 
 class PageFooter extends Component {
     constructor() {
         super();
-        this.text = 'Page footer';
+        this.text = 'Uniquers';
     }
 
     render() {
         return (
             <div className="PageFooter">
-            {this.text}
+                <div>
+                    <p>{this.text}</p>
+                    <img className="PageFooter-brandcode" src={codeImg} alt="brand" />
+                </div>
+
+                <div>
+                    <p>服务中心</p>
+                    <p className="PageFooter-subtext">服务热线</p>
+                </div>
+
+                <div>
+                    <p>帮助中心</p>
+                    <p className="PageFooter-subtext">购物流程</p>
+                    <p className="PageFooter-subtext">会员说明</p>
+                    <p className="PageFooter-subtext">品牌入住</p>
+                </div>
+
+                <div>
+                    <p>关于我们</p>
+                    <p className="PageFooter-subtext">介绍</p>
+                    <p className="PageFooter-subtext">联系我们</p>
+                    <p className="PageFooter-subtext">招贤纳士</p>
+                </div>
             </div>
         );
     }
